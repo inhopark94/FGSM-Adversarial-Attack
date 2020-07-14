f36149683e18f1b393569161ffa5e3a7d316993b
inho park <59054431+inhopark94@users.noreply.github.com>
2020-07-15 00:32:51 +0900

    Add files via upload

---
+++ WhiteBox.py
@@ -0,0 +1,154 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul  8 15:34:33 2020
+
+@author: ihpark
+"""
+
+from __future__ import print_function ## ??
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import datasets, transforms
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
+epsilons = [0, .05, .1, .15, .2, .25, .3]
+pretrained_model =  '../data/MNIST_net.pth'
+PATH = '../data/MNIST_net.pth'
+use_cuda=True
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
+        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
+        self.conv2_drop = nn.Dropout2d()
+        self.fc1 = nn.Linear(320, 50)
+        self.fc2 = nn.Linear(50, 10)
+
+    def forward(self, x):
+        x = F.relu(F.max_pool2d(self.conv1(x), 2))
+        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
+        x = x.view(-1, 320)
+        x = F.relu(self.fc1(x))
+        x = F.dropout(x, training=self.training)
+        x = self.fc2(x)
+        return F.log_softmax(x, dim=1)
+
+test_loader = torch.utils.data.DataLoader(
+    datasets.MNIST('../data', train=False, download=True, transform=transforms.Compose([
+            transforms.ToTensor(),
+            ])),
+        batch_size=1, shuffle=True)
+train_loader = torch.utils.data.DataLoader(
+    datasets.MNIST('../data', train=True, download=True, transform=transforms.Compose([
+            transforms.ToTensor(),
+            ])),
+        batch_size=16, shuffle=True)
+
+net =Net()
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(net.parameters(), lr=0.001)
+
+print("CUDA Available: ",torch.cuda.is_available())
+device = torch.device("cuda" if (use_cuda and torch.cuda.is_available()) else "cpu")
+
+
+model = net.to(device)
+classes = ('0','1','2','3','4','5','6','7','8','9')
+
+
+
+if not  os.path.isfile('../data/MNIST_net.pth') :
+    for epoch in range(50):  
+        running_loss = 0.0
+        for i, data in enumerate(train_loader, 0):
+            inputs, labels = data
+            inputs, labels= inputs.to(device), labels.to(device)
+            optimizer.zero_grad()
+            outputs = net(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+    
+    
+            running_loss += loss.item()
+            if i % 2000 == 1999:    
+                print('[%d, %5d] loss: %.3f' %
+                      (epoch + 1, i + 1, running_loss / 2000))
+                running_loss = 0.0
+    print('Finished Training')
+    torch.save(net.state_dict(),PATH)
+print('load pretrained data ')
+model.load_state_dict(torch.load(pretrained_model, map_location='cpu'))
+    
+model.eval()
+
+
+# FGSM ATTACK
+def fgsm_attack(image, epsilon, data_grad):
+    sign_data_grad = data_grad.sign()
+    perturbed_image = image + epsilon*sign_data_grad
+    perturbed_image = torch.clamp(perturbed_image, 0, 1)
+    return perturbed_image
+
+
+def test( model, device, test_loader, epsilon ):
+
+
+    correct = 0
+    adv_examples = []
+
+
+    for data, target in test_loader:
+        data, target = data.to(device), target.to(device)
+        data.requires_grad = True
+        output = model(data)
+        init_pred = output.max(1, keepdim=True)[1]
+
+        if init_pred.item() != target.item():
+            continue
+
+        loss = F.nll_loss(output, target)
+        model.zero_grad()
+        loss.backward()
+        data_grad = data.grad.data
+        perturbed_data = fgsm_attack(data, epsilon, data_grad)
+        output = model(perturbed_data)
+        final_pred = output.max(1, keepdim=True)[1]
+        if final_pred.item() == target.item():
+
+            if (epsilon == 0) and (len(adv_examples) < 5):
+                adv_ex = perturbed_data.squeeze().detach().cpu().numpy()
+                adv_examples.append( (init_pred.item(), final_pred.item(), adv_ex) )
+
+            if len(adv_examples) < 5:
+                adv_ex = perturbed_data.squeeze().detach().cpu().numpy()
+                adv_examples.append( (init_pred.item(), final_pred.item(), adv_ex) )
+
+    final_acc = correct/float(len(test_loader))
+    print("Epsilon: {}\tTest Accuracy = {} / {} = {}".format(epsilon, correct, len(test_loader), final_acc))
+
+    return final_acc, adv_examples
+
+accuracies = []
+examples = []
+
+for eps in epsilons:
+    acc, ex = test(model, device, test_loader, eps)
+    accuracies.append(acc)
+    examples.append(ex)
+
+
+plt.figure(figsize=(5,5))
+plt.plot(epsilons, accuracies, "*-")
+plt.yticks(np.arange(0, 1.1, step=0.1))
+plt.xticks(np.arange(0, .35, step=0.05))
+plt.title("Accuracy vs Epsilon")
+plt.xlabel("Epsilon")
+plt.ylabel("Accuracy")
+plt.show()
